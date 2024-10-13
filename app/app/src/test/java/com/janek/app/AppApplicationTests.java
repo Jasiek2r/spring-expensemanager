19af586fff9f21d6bd8b31888886e4755230ae3f
Jasiek2r <jasiek2r@gmail.com>
2024-10-13 13:50:01 +0200

    finally can get started

---
+++ app/app/src/test/java/com/janek/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janek.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
